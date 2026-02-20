b1ebbaa29f0fdc8228c3c4370dc57bc74c558a32
Oliver Marshall <oliver.marshall@ukaea.uk>
2026-02-20 15:54:14 +0000

    breeder blanket manager from optimised reactor example

---
+++ bluemira_st/blanket/manager.py
@@ -0,0 +1,10 @@
+from bluemira.base.reactor import ComponentManager
+from bluemira.geometry.face import BluemiraFace
+from bluemira_st.blanket.builder import BBBuilder
+
+class BB(ComponentManager):
+    """Breeding blanket component manager."""
+
+    def xz_face(self) -> BluemiraFace:
+        """Get the 2D xz face of the breeding blanket."""
+        return self.component().get_component("xz").get_component(BBBuilder.BB).shape
